687079ef3fc80a184394b34578498857c089fe4f
Kresno <kresno.19@gmail.com>
2019-01-07 13:28:55 +0700

    template admin tahap 1 fix

--- application/controllers/superadmin/Dashboard.php
+++ application/controllers/superadmin/Dashboard.php
@@ -20,7 +20,7 @@ class Dashboard extends CI_Controller
 
     $this->load->view('layout/superadmin/header');
     $this->load->view('layout/superadmin/sidebar', $header);
-    $this->load->view('dashboard/dashboard/index');
+    $this->load->view('superadmin/dashboard/index');
     $this->load->view('layout/superadmin/footer');
   }
 }
